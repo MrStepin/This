bccba66b42b29b00149000ef7693d4daa7c23896
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-03-23 21:01:42 +0300

    .

--- This/Program.cs
+++ This/Program.cs
@@ -10,6 +10,18 @@ namespace This
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter Max Width");
+            Screen.MaxWidht = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Max Height");
+            Screen.MaxHeight = Convert.ToDouble(Console.ReadLine());
+
+            Page page = new Page();
+            Console.WriteLine($"{page.Width} + {page.Height}");
+            Page pageWidth = new Page(5);
+            Console.WriteLine($"{pageWidth.Width} + {pageWidth.Height}");
+            Page pageWidthHeight = new Page(6,4, Screen.MaxWidht, Screen.MaxHeight);
+            Console.WriteLine($"{pageWidthHeight.Width} + {pageWidthHeight.Height}");
+            Console.ReadKey();
         }
     }
 }
---
+++ This/Screen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This
+{
+    class Screen
+    {
+        public static double MaxWidht;
+        public static double MaxHeight;
+    }
+}
